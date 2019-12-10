12568d536db42fcff67e7a418742714f6d29a7a1
Muharrem Tigdemir <muharrem.tigdemir@growthengineering.co.uk>
2019-12-10 15:01:55 +0000

    Spiral Framework initialize

---
+++ app/locale/ru/messages.en.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'GitHub'                       => 'GitHub',
+    'Exception'                    => 'Страница ошибки',
+    'Create Queue Task'            => 'Создать фоновую задачу',
+    'Application Metrics'          => 'Prometheus метрики',
+    'Website and Documentation'    => 'Документация',
+    'Welcome To Spiral'            => 'Добро пожаловать',
+    'Welcome to Spiral Framework'  => 'Вас приветствует Spiral Framework',
+    'This view file is located in' => 'Данный шаблон находится в файле',
+    'and rendered by'              => 'и вызван контроллером',
+];
